980a2c78149419f86ea088ff319d4b04063aeda5
adamjcoates <ajcoates0@gmail.com>
2022-06-16 21:26:30 -0700

    Added Functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import Navbar from "./Navbar";
+import Settings from "./pages/Settings";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <h1>New React Project</h1>
-    
-
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Settings />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
